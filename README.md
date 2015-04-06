5d6e7eff4b0511108cc8f115f14d5d938be630bc
Richard Osterloh <richard.osterloh@gmail.com>
2015-04-06 21:56:00 +0100

    initial commit

---
+++ README.md
@@ -0,0 +1,3 @@
+# [ng2-playground](https://github.com/rosterloh/ng2-playground)
+
+Project to begin playing with Angular2.0
---
+++ gulpfile.js
@@ -0,0 +1,75 @@
+var gulp = require('gulp');
+var $ = require('gulp-load-plugins')();
+var del = require('del');
+var pkg = require('./package');
+var browserSync = require('browser-sync');
+var reload = browserSync.reload;
+
+var PATHS = {
+    src: {
+      js: 'src/**/*.js',
+      html: 'src/**/*.html'
+    },
+    lib: [
+      'node_modules/gulp-traceur/node_modules/traceur/bin/traceur-runtime.js',
+      'node_modules/es6-module-loader/dist/es6-module-loader-sans-promises.src.js',
+      'node_modules/systemjs/lib/extension-register.js',
+      'node_modules/angular2/node_modules/zone.js/zone.js',
+      'node_modules/angular2/node_modules/zone.js/long-stack-trace-zone.js'
+    ]
+};
+
+gulp.task('clean', function(done) {
+  del(['dist'], done);
+});
+
+gulp.task('js', function () {
+    return gulp.src(PATHS.src.js)
+        .pipe($.rename({extname: ''})) //hack, see: https://github.com/sindresorhus/gulp-traceur/issues/54
+        .pipe($.plumber())
+        .pipe($.traceur({
+            modules: 'instantiate',
+            moduleName: true,
+            annotations: true,
+            types: true,
+            memberVariables: true
+        }))
+        .pipe($.rename({extname: '.js'})) //hack, see: https://github.com/sindresorhus/gulp-traceur/issues/54
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('html', function () {
+    return gulp.src(PATHS.src.html)
+        .pipe(gulp.dest('dist'));
+});
+
+gulp.task('libs', ['angular2'], function () {
+    return gulp.src(PATHS.lib)
+        .pipe(gulp.dest('dist/lib'));
+});
+
+gulp.task('angular2', function () {
+
+  var buildConfig = {
+    paths: {
+      "angular2/*": "node_modules/angular2/es6/prod/*.es6",
+      "rx/*": "node_modules/angular2/node_modules/rx/*.js"
+    }
+  };
+
+  var Builder = require('systemjs-builder');
+  var builder = new Builder(buildConfig);
+
+  return builder.build('angular2/angular2', 'dist/lib/angular2.js', {});
+});
+
+gulp.task('default', ['js', 'html', 'libs'], function() {
+  browserSync({
+    notify: false,
+    logPrefix: pkg.name,
+    server: ['dist']
+  });
+
+  gulp.watch(PATHS.src.html, reload);
+  gulp.watch(PATHS.src.js, reload);
+});
