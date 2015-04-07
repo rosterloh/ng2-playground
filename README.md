fe5a463a1f353b1663e5f49a47044307bf2168ab
Richard Osterloh <richard.osterloh@gmail.com>
2015-04-07 08:56:09 +0100

    testing adding material angular

--- README.md
+++ README.md
@@ -1,3 +1,5 @@
 # [ng2-playground](https://github.com/rosterloh/ng2-playground)
 
+[![Dependency Status](https://david-dm.org/rosterloh/ng2-playground.svg?style=flat-square)](https://david-dm.org/rosterloh/ng2-playground) [![devDependency Status](https://david-dm.org/rosterloh/ng2-playground/dev-status.svg?style=flat-square)](https://david-dm.org/rosterloh/ng2-playground#info=devDependencies)
+
 Project to begin playing with Angular2.0
--- gulpfile.js
+++ gulpfile.js
@@ -8,14 +8,16 @@ var reload = browserSync.reload;
 var PATHS = {
     src: {
       js: 'src/**/*.js',
-      html: 'src/**/*.html'
+      html: 'src/**/*.html',
+      css: 'node_modules/angular-material/angular-material.css'
     },
     lib: [
       'node_modules/gulp-traceur/node_modules/traceur/bin/traceur-runtime.js',
       'node_modules/es6-module-loader/dist/es6-module-loader-sans-promises.src.js',
       'node_modules/systemjs/lib/extension-register.js',
       'node_modules/angular2/node_modules/zone.js/zone.js',
-      'node_modules/angular2/node_modules/zone.js/long-stack-trace-zone.js'
+      'node_modules/angular2/node_modules/zone.js/long-stack-trace-zone.js',
+      'node_modules/angular-material/angular-material.js'
     ]
 };
 
@@ -43,6 +45,11 @@ gulp.task('html', function () {
         .pipe(gulp.dest('dist'));
 });
 
+gulp.task('css', function () {
+    return gulp.src(PATHS.src.css)
+        .pipe(gulp.dest('dist'));
+});
+
 gulp.task('libs', ['angular2'], function () {
     return gulp.src(PATHS.lib)
         .pipe(gulp.dest('dist/lib'));
@@ -63,7 +70,7 @@ gulp.task('angular2', function () {
   return builder.build('angular2/angular2', 'dist/lib/angular2.js', {});
 });
 
-gulp.task('default', ['js', 'html', 'libs'], function() {
+gulp.task('default', ['js', 'html', 'css', 'libs'], function() {
   browserSync({
     notify: false,
     logPrefix: pkg.name,
