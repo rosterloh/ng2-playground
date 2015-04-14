6b6fda7cf92fc73effd6565473b91c1dc65bc58a
Richard Osterloh <richard.osterloh@gmail.com>
2015-04-14 08:10:32 +0100

    updates

--- gulpfile.js
+++ gulpfile.js
@@ -7,17 +7,18 @@ var reload = browserSync.reload;
 
 var PATHS = {
     src: {
-      js: 'src/**/*.js',
-      html: 'src/**/*.html',
-      css: 'node_modules/angular-material/angular-material.css'
+        root: 'src',
+        js: 'src/**/*.js',
+        html: 'src/**/*.html',
+        css: 'node_modules/angular-material/angular-material.css'
     },
     lib: [
-      'node_modules/gulp-traceur/node_modules/traceur/bin/traceur-runtime.js',
-      'node_modules/es6-module-loader/dist/es6-module-loader-sans-promises.src.js',
-      'node_modules/systemjs/lib/extension-register.js',
-      'node_modules/angular2/node_modules/zone.js/zone.js',
-      'node_modules/angular2/node_modules/zone.js/long-stack-trace-zone.js',
-      'node_modules/angular-material/angular-material.js'
+        'node_modules/gulp-traceur/node_modules/traceur/bin/traceur-runtime.js',
+        'node_modules/es6-module-loader/dist/es6-module-loader-sans-promises.src.js',
+        'node_modules/systemjs/lib/extension-register.js',
+        'node_modules/angular2/node_modules/zone.js/zone.js',
+        'node_modules/angular2/node_modules/zone.js/long-stack-trace-zone.js',
+        'node_modules/angular-material/angular-material.js'
     ]
 };
 
@@ -29,7 +30,9 @@ gulp.task('js', function () {
     return gulp.src(PATHS.src.js)
         .pipe($.rename({extname: ''})) //hack, see: https://github.com/sindresorhus/gulp-traceur/issues/54
         .pipe($.plumber())
+        .pipe($.sourcemaps.init())
         .pipe($.traceur({
+            sourceMaps: true,
             modules: 'instantiate',
             moduleName: true,
             annotations: true,
@@ -37,6 +40,7 @@ gulp.task('js', function () {
             memberVariables: true
         }))
         .pipe($.rename({extname: '.js'})) //hack, see: https://github.com/sindresorhus/gulp-traceur/issues/54
+        .pipe($.sourcemaps.write('.', {sourceRoot: PATHS.src.root}))
         .pipe(gulp.dest('dist'));
 });
 
@@ -47,7 +51,8 @@ gulp.task('html', function () {
 
 gulp.task('css', function () {
     return gulp.src(PATHS.src.css)
-        .pipe(gulp.dest('dist'));
+        .pipe($.size({showFiles: true, gzip: true}))
+        .pipe(gulp.dest('dist/lib'));
 });
 
 gulp.task('libs', ['angular2'], function () {
