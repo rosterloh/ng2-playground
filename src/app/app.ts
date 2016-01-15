5a10e25e59d242014631530ff6088c24e8c5faf7
Richard Osterloh <richard.osterloh@gmail.com>
2016-01-15 10:28:57 +0100

    Update to Angular beta and ng2-material

--- src/app/app.ts
+++ src/app/app.ts
@@ -1,98 +1,29 @@
-/// <reference path="../typings/_custom.d.ts" />
+import {Component, View} from 'angular2/core';
+import {ViewEncapsulation} from 'angular2/core';
+import {FORM_PROVIDERS} from 'angular2/common';
+import {Login} from './login/login';
+import {Home} from './home/home';
+import {NavigationService} from './navigation/navigation';
+import {MATERIAL_DIRECTIVES, MATERIAL_PROVIDERS} from 'ng2-material/all';
+import {ROUTER_DIRECTIVES, RouteConfig} from 'angular2/router';
 
-import {Directive, Component, View, ElementRef} from 'angular2/angular2';
-import {Location, RouteConfig, RouterLink, Router} from 'angular2/router';
-import {Http, Headers} from 'angular2/http';
-
-import {CORE_DIRECTIVES, FORM_DIRECTIVES} from 'angular2/angular2';
-import {ROUTER_DIRECTIVES} from 'angular2/router';
-
-import {LoggedInRouterOutlet} from './LoggedInOutlet';
-import {Home} from '../components/home/home';
-import {Login} from '../components/login/login';
-import {Signup} from '../components/signup/signup';
-
-let template = require('./app.html');
-
-/*
- * App Component
- * Top Level Component
- */
 @Component({
-  // The selector is what angular internally uses
-  // for `document.querySelectorAll(selector)` in our index.html
-  // where, in this case, selector is the string 'app'
-  selector: 'app', // <app></app>
-  // We need to tell Angular's compiler which directives are in our template.
-  // Doing so will allow Angular to attach our behavior to an element
-  directives: [ CORE_DIRECTIVES, FORM_DIRECTIVES, ROUTER_DIRECTIVES, LoggedInRouterOutlet ],
-  // Our list of styles in our component. We may add more to compose many styles together
-  styles: [`
-    .title {
-      font-family: Arial, Helvetica, sans-serif;
-    }
-    main {
-      padding: 1em;
-    }
-  `],
-  // Every Angular template is first compiled by the browser before Angular runs it's compiler
-  template: template
+  selector: 'app',
+  providers: [ ...FORM_PROVIDERS, NavigationService, MATERIAL_PROVIDERS ],
+  directives: [ MATERIAL_DIRECTIVES, ROUTER_DIRECTIVES ],
+  pipes: [],
+  styles: [
+    require('../assets/css/index.scss')
+  ],
+  encapsulation: ViewEncapsulation.None,
+  template: require('./app.html')
 })
 @RouteConfig([
-  { path: '/',       redirectTo: '/home' },
-  { path: '/home',   as: 'Home',   component: Home },
-  { path: '/login',  as: 'Login',  component: Login },
-  { path: '/signup', as: 'Signup', component: Signup }
+  {path: '/login', component: Login, name: 'Login', data: {title: 'Login Page'}},
+  {path: '/home', component: Home, name: 'Home', data: {title: 'Home Page'}},
+  {path: '/**', redirectTo: ['Login']}
 ])
 export class App {
-  // These are member type
-  title: string;
-  data: Array<any> = []; // default data
-  // TypeScript public modifiers
-  constructor(public router: Router, public http: Http) {
-    this.title = 'Angular 2';
+  constructor(public navigation: NavigationService) {
   }
-
-  onInit() {
-    // Our API
-    // npm run express-install
-    // npm run express
-
-    const BASE_URL = 'http://localhost:3001';
-    const TODO_API_URL = '/api/todos';
-    const JSON_HEADERS = new Headers();
-
-    JSON_HEADERS.append('Accept', 'application/json');
-    JSON_HEADERS.append('Content-Type', 'application/json');
-
-    this.http
-      .get(BASE_URL + TODO_API_URL, {
-        headers: JSON_HEADERS
-      })
-      .map(res => res.json())
-      .subscribe(
-        // onNext callback
-        data => this.serverData(data),
-        // onError callback
-        err  => this.errorMessage(err),
-        // onComplete callback
-        ()   => console.log('complete')
-      );//end http
-
-  }
-
-  serverData(data) {
-    console.log('data', data);
-    this.data = data;
-  }//serverData
-
-  errorMessage(err) {
-    console.info(`${'\n'
-      } // You must run these commands for the Http API to work in another process ${'\n'
-      } npm run express-install ${'\n'
-      } npm run express
-    `);
-    console.error(err);
-  }//errorMessage
-
 }
--- webpack.config.js
+++ webpack.config.js
@@ -3,47 +3,38 @@
 /*
  * Helper: root(), and rootDir() are defined at the bottom
  */
-var sliceArgs = Function.prototype.call.bind(Array.prototype.slice);
-var toString  = Function.prototype.call.bind(Object.prototype.toString);
 var path = require('path');
 var webpack = require('webpack');
-// Webpack Plugins
-var CommonsChunkPlugin = webpack.optimize.CommonsChunkPlugin;
+var CopyWebpackPlugin  = require('copy-webpack-plugin');
+var HtmlWebpackPlugin  = require('html-webpack-plugin');
+var ENV = process.env.ENV = process.env.NODE_ENV = 'development';
+
+var metadata = {
+  title: 'Angular2 Playground',
+  baseUrl: '/',
+  host: 'localhost',
+  port: 3000,
+  ENV: ENV
+};
 
 /*
  * Config
  */
 module.exports = {
-  devtool: 'source-map', // for faster builds use 'eval'
+  // static data for index.html
+  metadata: metadata,
+  // for faster builds use 'eval'
+  devtool: 'source-map',
   debug: true,
 
-  // our Webpack Development Server config
-  devServer: {
-    historyApiFallback: true,
-    contentBase: 'src/public',
-    publicPath: '/__build__'
-  },
-
-  entry: {
-    'angular2': [
-      // group angular2 deps into the angular2.js file
-      'core-js',
-      'rxjs',
-      'zone.js',
-      'reflect-metadata',
-      'angular2/angular2',
-      'angular2/core',
-      'angular2/router',
-      'angular2/http'
-    ],
-    'app': './src/app/bootstrap' // our angular app
-  },
+  // our angular app
+  entry: { 'vendor': './src/vendor.ts', 'main': './src/main.ts' },
 
   // Config for our build files
   output: {
-    path: root('__build__'),
-    filename: '[name].js',
-    sourceMapFilename: '[name].js.map',
+    path: root('dist'),
+    filename: '[name].bundle.js',
+    sourceMapFilename: '[name].map',
     chunkFilename: '[id].chunk.js'
   },
 
@@ -52,40 +43,80 @@ module.exports = {
   },
 
   module: {
+    preLoaders: [{ test: /\.ts$/, loader: 'tslint-loader', exclude: [/node_modules/] }],
     loaders: [
-      // Support for *.json files.
-      { test: /\.json$/,  loader: 'json' },
-
-      // Support for CSS as raw text
-      { test: /\.css$/,   loader: 'raw' },
-
-      // support for .html as raw text
-      { test: /\.html$/,  loader: 'raw' },
-
       // Support for .ts files.
       {
         test: /\.ts$/,
-        loader: 'ts',
-        query: { 'ignoreDiagnostics': [ 2403 ] }, // 2403 -> Subsequent variable declarations
-        exclude: [ /\.spec\.ts$/, /\.e2e\.ts$/, /node_modules/ ]
-      }
+        loader: 'ts-loader',
+        query: {
+          'ignoreDiagnostics': [
+            2403, // 2403 -> Subsequent variable declarations
+            2300, // 2300 -> Duplicate identifier
+            2374, // 2374 -> Duplicate number index signature
+            2375  // 2375 -> Duplicate string index signature
+          ]
+        },
+        exclude: [ /\.(spec|e2e)\.ts$/, /node_modules\/(?!(ng2-.+))/ ]
+      },
+
+      // Support for *.json files.
+      { test: /\.json$/,  loader: 'json-loader' },
+
+      // Support for CSS as raw text
+      { test: /\.css$/,   loader: 'raw-loader' },
+
+      {
+        test: /\.scss$/,
+        exclude: /node_modules/,
+        loader: 'raw-loader!sass-loader'
+      },
+
+      // support for .html as raw text
+      { test: /\.html$/,  loader: 'raw-loader' }
     ]
   },
 
   plugins: [
-    new CommonsChunkPlugin({ name: 'angular2', filename: 'angular2.js', minChunks: Infinity }),
-    new CommonsChunkPlugin({ name: 'common',   filename: 'common.js' })
-  ]
+    new webpack.optimize.OccurenceOrderPlugin(true),
+    new webpack.optimize.CommonsChunkPlugin({ name: 'vendor', filename: 'vendor.bundle.js', minChunks: Infinity }),
+    // static assets
+    new CopyWebpackPlugin([ { from: 'src/assets', to: 'assets' } ]),
+    // generating html
+    new HtmlWebpackPlugin({ template: 'src/index.html', inject: false }),
+    // replace
+    new webpack.DefinePlugin({
+      'process.env': {
+        'ENV': JSON.stringify(metadata.ENV),
+        'NODE_ENV': JSON.stringify(metadata.ENV)
+      }
+    })
+  ],
+
+  // Other module loader config
+  tslint: {
+    emitErrors: false,
+    failOnHint: false
+  },
+  // our Webpack Development Server config
+  devServer: {
+    port: metadata.port,
+    host: metadata.host,
+    historyApiFallback: true,
+    watchOptions: { aggregateTimeout: 300, poll: 1000 }
+  },
+  // we need this due to problems with es6-shim
+  node: {global: 'window', progress: false, crypto: 'empty', module: false, clearImmediate: false, setImmediate: false}
 };
 
 // Helper functions
 
 function root(args) {
-  args = sliceArgs(arguments, 0);
+  args = Array.prototype.slice.call(arguments, 0);
   return path.join.apply(path, [__dirname].concat(args));
 }
 
 function rootNode(args) {
-  args = sliceArgs(arguments, 0);
+  args = Array.prototype.slice.call(arguments, 0);
   return root.apply(path, ['node_modules'].concat(args));
 }
