8dd4aae9e72a8a3b9cc63f48620d43fd8fa73ce3
Richard Osterloh <richard.osterloh@gmail.com>
2016-04-01 10:11:05 +0200

    Updating to latest from AngularClass

--- src/vendor.ts
+++ src/vendor.ts
@@ -1,33 +1,36 @@
-// Polyfills
-import 'es6-shim';
-// (these modules are what are in 'angular2/bundles/angular2-polyfills' so don't use that here)
-import 'es6-promise';
-import 'reflect-metadata';
-import 'zone.js/lib/browser/zone-microtask';
+// For vendors for example jQuery, Lodash, angular2-jwt just import them here unless you plan on
+// chunking vendors files for async loading. You would need to import the async loaded vendors
+// at the entry point of the async loaded file. Also see custom-typings.d.ts as you also need to
+// run `typings install x` where `x` is your module
 
-if ('production' !== process.env.ENV) {
-  // Reflect Polyfill
-  require('es7-reflect-metadata/dist/browser');
-  Error['stackTraceLimit'] = Infinity;
-  Zone['longStackTraceZone'] = require('zone.js/lib/zones/long-stack-trace.js');
-}
-
-if ('production' === process.env.ENV) {
-  // Reflect with es7-reflect-metadata/reflect-metadata is added
-  // by webpack.prod.config ProvidePlugin
-  let ngCore = require('angular2/core');
-  ngCore.enableProdMode();
-}
 // Angular 2
 import 'angular2/platform/browser';
 import 'angular2/platform/common_dom';
-import 'angular2/router';
-import 'angular2/http';
 import 'angular2/core';
+import 'angular2/common';
+import 'angular2/http';
+import 'angular2/router';
 
 // RxJS
-import 'rxjs';
+import 'rxjs/add/operator/map';
+import 'rxjs/add/operator/mergeMap';
 
-// Other vendors for example jQuery, Lodash, angular2-jwt
-import 'ng2-material/source/all';
-import 'angular2-jwt';
+// Angular 2 Material 2
+// TODO(gdi2290): uncomment when material is fixed
+// import '@angular2-material/sidenav';
+// import '@angular2-material/toolbar';
+// import '@angular2-material/button';
+// import '@angular2-material/checkbox';
+// import '@angular2-material/radio';
+// import '@angular2-material/progress-circle';
+// import '@angular2-material/card';
+// look in platform/directives and platform/providers
+
+if ('production' === ENV) {
+  // Production
+
+
+} else {
+  // Development
+
+}
